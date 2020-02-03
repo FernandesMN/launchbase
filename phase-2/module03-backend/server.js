99608f4203331768a137b283d56b9ce6fbf22d50
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-03 06:28:02 -0300

    finished template engine and configured .njk files

--- phase-2/module03-backend/server.js
+++ phase-2/module03-backend/server.js
@@ -5,18 +5,18 @@ const server = express();
 
 server.use(express.static('public'));
 
-server.set("view engine", "html");
+server.set("view engine", "njk");
 
 nunjucks.configure("views", {
     express: server
 });
 
 server.get('/', function(req, res){
-    return res.render("index");
+    return res.render("about");
 });
 
 server.get('/portfolio', function(req, res) {
-    return res.render('portfolio')
+    return res.render("portfolio")
 });
 
 server.listen(5000, function(){
