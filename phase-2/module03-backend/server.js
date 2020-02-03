21749ab0146d106cf8e5e323046881626e192c2f
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-03 07:45:10 -0300

    aligning elements and updating page about

--- phase-2/module03-backend/server.js
+++ phase-2/module03-backend/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public'));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 });
 
 server.get('/', function(req, res){
-    return res.render("about");
+    const data = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/51912507?s=400&u=4473f830348c529299e0f1d9265a087222ad2546&v=4",
+        name: "Matheus Fernandes",
+        role: 'Diretor Administrativo Financeiro - <a href="https://www.catiuespi.com.br/" target="_blank">CATI</a>',
+        description: 'Estudante - Ciência da Computação na Universidade Estadual do Piauí(<a href="https://www.uespi.br/site/" target="_blank">UESPI</a>)',
+        links: [
+            {name: 'Github', url: 'https://github.com/FernandesMN'},
+            {name: 'Instagram', url: 'https://www.instagram.com/eifernandess/'},
+            {name: 'Linkedin', url: 'https://www.linkedin.com/in/matheus-fernandes-nascimento/'},
+        ]
+    }
+
+    return res.render("about", {about: data});
 });
 
 server.get('/portfolio', function(req, res) {
