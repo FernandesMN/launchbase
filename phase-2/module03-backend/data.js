dca85ef24820e4bdfa2c58b3a585e374d5e5c9de
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-04 06:58:40 -0300

    featured videos

--- phase-2/module03-backend/data.js
+++ phase-2/module03-backend/data.js
@@ -3,41 +3,47 @@ module.exports = [
         id: "2alg7MQ6_sI",
         title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
         duration: "33 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
 
     {
         id: "GykTLqODQuU",
         title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
         duration: "57 min",
-        price: "Free"
+        price: "Free",
+        featured: false
     },
 
     {
         id: "HN1UjzRSdBk",
         title: "Desvendando o CSS Grid na prática | Mayk Brito",
         duration: "36 min",
-        price: "Free"
+        price: "Free",
+        featured: false
     },
 
     {
         id: "mcd7lqpUzIA",
         title: "Criando contribuição open-source no Github do zero! | Diego Fernandes",
         duration: "42 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
 
     {
         id: "kg2-SMolAV0",
         title: "TypeScript - O que é e quais os seus benefícios? | Diego Fernandes",
         duration: "15 min",
-        price: "Free"
+        price: "Free",
+        featured: false
     },
 
     {
         id: "QfcozcbDhNM",
         title: "Podcast FalaDev #11 - Técnicas de estudo e aprendizado tradicional",
         duration: "43 min",
-        price: "Free"
+        price: "Free",
+        featured: false
     }
 ]
