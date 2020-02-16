ecf89efc786f0cef3d0dc847f9a11aa1f10d40a2
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-16 09:48:35 -0300

    folder deleted and comments added

--- phase-2/module03-backend/server.js
+++ phase-2/module03-backend/server.js
@@ -1,19 +1,24 @@
+//importando dependências
 const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+//importando dados
 const videos = require('./data');
 
+//para usar arquivos de estilização
 server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
+//configurações do nunjucks
 nunjucks.configure("views", {
     express: server,
     autoescape: false,
     noCache: true
 });
 
+//rota para pegar e exibir os dados na página inicial
 server.get('/', function(req, res){
     const data = {
         avatar_url: "https://avatars2.githubusercontent.com/u/51912507?s=400&u=4473f830348c529299e0f1d9265a087222ad2546&v=4",
@@ -34,7 +39,7 @@ server.get('/portfolio', function(req, res) {
     return res.render("portfolio", {items: videos});
 });
 
-
+//rota para a página única de vídeo
 server.get('/video', function(req,res) {
     const id = req.query.id;
 
@@ -49,6 +54,7 @@ server.get('/video', function(req,res) {
     return res.render("video", {item: video});
 })
 
+//ouvindo porta
 server.listen(5000, function(){
     console.log("server is running");
 });
