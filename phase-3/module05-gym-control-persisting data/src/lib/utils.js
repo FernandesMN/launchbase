b99aee674d08b2f0d4070e2182de6e71b125a25f
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-25 08:43:49 -0300

    module05: refactoring folder structure

---
+++ phase-3/module05-gym-control-persisting data/src/lib/utils.js
@@ -0,0 +1,31 @@
+module.exports = {
+    age: function age(timestamp) {
+        const today = new Date();
+        const birthDay = new Date(timestamp);
+    
+        let age = today.getFullYear() - birthDay.getFullYear();
+        let month = today.getMonth() - birthDay.getMonth();
+    
+        if(month < 0 || month == 0 && today.getDate() <= birthDay.getDate()) {
+            age = age - 1;
+        }
+    
+        return age;
+    },
+
+    date: function date(timestamp) {
+        const date = new Date(timestamp);
+
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        return {
+            day,
+            month,
+            year,
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`
+        };
+    }
+}
