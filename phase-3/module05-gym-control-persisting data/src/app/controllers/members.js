ba7e7973e852c000f5bad273a5705458aa52ccc8
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-27 08:41:29 -0300

    module05: adjusting the members part

--- phase-3/module05-gym-control-persisting data/src/app/controllers/members.js
+++ phase-3/module05-gym-control-persisting data/src/app/controllers/members.js
@@ -1,32 +1,49 @@
-const { age, date } = require('../../lib/utils');
+const Member = require('../models/Member');
+const { date, age } = require('../../lib/utils');
 
 module.exports = {
     index(req,res) {
-        return res.render("members/index");
+        Member.all(function(members) {
+            return res.render("members/index", {members});
+        });
     },
 
     create(req,res) {
         return res.render("members/create");
     },
 
-    post(req,res) {                                                                                     
+    post(req,res) {   
         const keys = Object.keys(req.body);
     
         for(key of keys) {
             if(req.body[key] == "") {
                 return res.send("Please, fill all fields.")
             }
-        }
-    
-        return
+        }        
+        
+        Member.create(req.body, function(teacher) {
+            return res.redirect(`/members/${teacher.id}`);
+        });
     },
 
     show(req,res) {
-        return
+        Member.find(req.params.id, function(member) {
+            if(!member) return res.send("Member not found!");
+
+            member.birth = date(member.birth).birthDay;
+
+            return res.render("members/show", {member});
+        });
     },
 
     edit(req,res) {
-        return
+        Member.find(req.params.id, function(member) {
+            if(!member) return res.send("Member not found!");
+
+            member.birth = date(member.birth).iso;
+
+            return res.render("members/edit", {member});
+        });
     },
 
     put(req,res) {
@@ -38,10 +55,15 @@ module.exports = {
             }
         }
 
-        return
+        Member.update(req.body, function() {
+            return res.redirect(`/members/${req.body.id}`)
+        });
     },
 
     delete(req,res) {
-        return
+        Member.delete(req.body.id, function() {
+            return res.redirect("/members")
+        });
     }  
 };
+
